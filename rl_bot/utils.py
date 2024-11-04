17ea7e13d6b64a0f48c81b781d86031a9aa9d1dd
hanhvn <vonhathanh@gmail.com>
2024-11-04 07:53:31 +0700

    use another epsilon decay algo

--- rl_bot/utils.py
+++ rl_bot/utils.py
@@ -1,5 +1,8 @@
+import math
+
+
 def linear_schedule(start_e: float, end_e: float, duration: int, t: int) -> float:
     # linearly reduce espilon from start_e to end_e in "duration" timestep
     # t: current timestep in the training loop
-    slope = (end_e - start_e) / duration
-    return max(slope * t + start_e, end_e)
+    eps_threshold = end_e + (start_e - end_e) * math.exp(-1. * t / duration)
+    return eps_threshold
