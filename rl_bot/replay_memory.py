761d865031c7ebff43ecf09a88f12cfeb336b09b
hanhvn <vonhathanh@gmail.com>
2024-11-07 15:53:09 +0700

    test PriporityMemory

--- rl_bot/replay_memory.py
+++ rl_bot/replay_memory.py
@@ -1,6 +1,8 @@
 import numpy as np
 import torch
 
+from rl_bot.segment_tree import *
+
 
 class ReplayMemory:
 
@@ -39,3 +41,49 @@ class ReplayMemory:
             "next_states": torch.tensor(self.next_states[indices]).to(self.device, dtype=torch.float32),
             "dones": torch.tensor(self.dones[indices]).to(self.device, dtype=torch.float32)
         }
+
+class PriporityMemory(ReplayMemory):
+    """
+    Priority Memory implementation of the paper: PRIORITIZED EXPERIENCE REPLAY
+    This version uses sum tree (segment tree) as in the paper reference
+    """
+    def __init__(self, size: int, observation_dim: tuple, device):
+        super().__init__(size, observation_dim, device)
+        self.priorities = np.zeros(size)
+        self.sum_tree = np.zeros(size*4)
+        init(self.sum_tree, self.priorities, 0, size-1)
+
+    def store(self, memory: tuple):
+        *m, priority = memory
+        update_tree(self.sum_tree, self.priorities, priority - self.priorities[self.ptr], self.ptr)
+        super().store(m)
+
+    def sample(self, batch_size: int):
+        pass
+
+
+if __name__ == '__main__':
+    # test PriporityMemory
+    obs_shape = (4, 1)
+    size = 3
+    m = PriporityMemory(size, obs_shape, "cpu")
+    print(m.sum_tree)
+    print(m.priorities)
+
+    state = np.zeros(obs_shape)
+
+    m.store((state, np.float32(0), np.float32(0), state, np.int64(0), 10))
+    print(m.sum_tree)
+    print(m.priorities)
+
+    m.store((state, np.float32(0), np.float32(0), state, np.int64(5), -15))
+    print(m.sum_tree)
+    print(m.priorities)
+
+    m.store((state, np.float32(0), np.float32(0), state, np.int64(5), -3))
+    print(m.sum_tree)
+    print(m.priorities)
+
+    m.store((state, np.float32(0), np.float32(0), state, np.int64(5), -4))
+    print(m.sum_tree)
+    print(m.priorities)
