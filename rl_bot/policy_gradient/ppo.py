8ec9d11bec0807da374f2f0ed7043a43c64ff57d
hanhvn <vonhathanh@gmail.com>
2024-11-27 13:16:55 +0700

    move ActionNormalizer to separate class

--- rl_bot/policy_gradient/ppo.py
+++ rl_bot/policy_gradient/ppo.py
@@ -10,36 +10,9 @@ from torch import nn, optim
 from torch.distributions import Normal
 from torch.utils.tensorboard import SummaryWriter
 
+from rl_bot.action_normalizer import ActionNormalizer
 from rl_bot.utils import init_uniformly
 
-class ActionNormalizer(gym.ActionWrapper):
-    """Rescale and relocate the actions."""
-
-    def action(self, action: np.ndarray) -> np.ndarray:
-        """Change the range (-1, 1) to (low, high)."""
-        low = self.action_space.low
-        high = self.action_space.high
-
-        scale_factor = (high - low) / 2
-        reloc_factor = high - scale_factor
-
-        action = action * scale_factor + reloc_factor
-        action = np.clip(action, low, high)
-
-        return action
-
-    def reverse_action(self, action: np.ndarray) -> np.ndarray:
-        """Change the range (low, high) to (-1, 1)."""
-        low = self.action_space.low
-        high = self.action_space.high
-
-        scale_factor = (high - low) / 2
-        reloc_factor = high - scale_factor
-
-        action = (action - reloc_factor) / scale_factor
-        action = np.clip(action, -1.0, 1.0)
-
-        return action
 
 class Actor(nn.Module):
     def __init__(
